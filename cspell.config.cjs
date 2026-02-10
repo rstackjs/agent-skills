6d4ebc9251f7936eade9cb5e2ca2ffcc198089de
Soon <a1149625757@126.com>
2026-02-10 12:08:02 +0800

    chore(infra/cspell): add cspell spell checking (#12)

---
+++ cspell.config.cjs
@@ -0,0 +1,30 @@
+const { banWords } = require('cspell-ban-words');
+
+module.exports = {
+  version: '0.2',
+  language: 'en',
+  files: ['**/{*,.*}/**/*.{ts,tsx,js,jsx,md,mdx,yml,yaml}'],
+  enableFiletypes: ['mdx', 'github-actions-workflow'],
+  ignoreRegExpList: [
+    // ignore markdown anchors such as [fooBar](#foobar)
+    '#.*?\\)',
+  ],
+  ignorePaths: [
+    'dist',
+    'dist-*',
+    'compiled',
+    'coverage',
+    'node_modules',
+    'pnpm-lock.yaml',
+    'skills/**/scripts',
+  ],
+  flagWords: banWords,
+  dictionaries: ['dictionary'],
+  dictionaryDefinitions: [
+    {
+      name: 'dictionary',
+      path: './scripts/dictionary.txt',
+      addWords: true,
+    },
+  ],
+};
